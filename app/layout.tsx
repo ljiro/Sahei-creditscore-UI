6bacbb5329a8bc2eb9514206be2117fd3dbd1ec4
Liam Morales <dredknives04@gmail.com>
2025-06-06 17:59:02 +0800

    Changed next.js decriptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Pilot App',
+  description: 'By Sahei Interns',
   generator: 'v0.dev',
 }
 
