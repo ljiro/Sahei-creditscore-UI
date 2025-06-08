dfe12611be11e3f47c165a24a135a2712f678f20
Liam Morales <dredknives04@gmail.com>
2025-06-08 14:26:54 +0800

    Change routes

--- app/page.tsx
+++ app/page.tsx
@@ -47,6 +47,6 @@ export default function Page() {
       return <AdminLoginPage />
     // Add other routes as needed
     default:
-      return <ClientManagemente />
+      return <LoanPage/>
   }
 }
