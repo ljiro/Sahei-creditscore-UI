6f5a6ac429b7fd417ac5531632fca089965f462a
Lou <loudiamondmorados162@gmail.com>
2025-06-23 11:32:57 +0800

    Update placeholder text for search inputs in Loans and User Management pages

--- app/david/loans/page.tsx
+++ app/david/loans/page.tsx
@@ -643,7 +643,7 @@ export default function LoansPage() {
               <div className="relative flex-1 max-w-sm">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
                 <Input
-                  placeholder="Search loans..."
+                  placeholder="Search Loans..."
                   value={searchText}
                   onChange={(e) => setSearchText(e.target.value)}
                   className="pl-10"
--- app/david/users/page.tsx
+++ app/david/users/page.tsx
@@ -661,7 +661,7 @@ export default function UserManagementPage() {
               <div className="relative flex-1 max-w-sm">
                 <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
                 <Input
-                  placeholder="Search loans..."
+                  placeholder="Search Users..."
                   value={searchText}
                   onChange={(e) => setSearchText(e.target.value)}
                   className="pl-10"
